c07eb3bd71850288b68627086108e4c1f1c4fdd8
Dániel Buga <bugadani@gmail.com>
2016-02-05 12:40:15 +0100

    Refactor

---
+++ src/Constructor.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Factory;
+
+use Factory\Exceptions\InstantiationException;
+
+class Constructor
+{
+    /**
+     * @var \ReflectionClass
+     */
+    private $reflector;
+
+    /**
+     * @var string
+     */
+    private $className;
+
+    /**
+     * @var array
+     */
+    private $parameters = [];
+
+    /**
+     * @var Factory
+     */
+    private $factory;
+
+    /**
+     * @var array|null
+     */
+    private $constructorArgs;
+
+    /**
+     * @var callable[]
+     */
+    private $callbacks = [];
+
+    public function __construct(Factory $factory, $className)
+    {
+        $this->className = $className;
+        $this->factory   = $factory;
+    }
+
+    public function setParameters(array $parameters)
+    {
+        $this->parameters = $parameters;
+    }
+
+    public function addCallback(callable $callback)
+    {
+        $this->callbacks[] = $callback;
+    }
+
+    /**
+     * @param \ReflectionParameter[] $constructorParameters
+     * @param array                  $parameters
+     *
+     * @return mixed
+     */
+    private function prepareParameters($constructorParameters, $parameters)
+    {
+        $return  = [];
+        $missing = [];
+
+        foreach ($constructorParameters as $constructorArg) {
+            $parameterName     = $constructorArg->getName();
+            $parameterPosition = $constructorArg->getPosition();
+
+            if (isset($parameters[ $parameterName ])) {
+                $return[] = $parameters[ $parameterName ];
+            } else if (isset($parameters[ $parameterPosition ])) {
+                $return[] = $parameters[ $parameterPosition ];
+            } else if (isset($this->parameters[ $parameterName ])) {
+                $return[] = $this->parameters[ $parameterName ];
+            } else if (isset($this->parameters[ $parameterPosition ])) {
+                $return[] = $this->parameters[ $parameterPosition ];
+            } else if ($constructorArg->getClass() !== null) {
+                try {
+                    $return[] = $this->factory->get($constructorArg->getClass()->getName());
+                } catch (InstantiationException $e) {
+                    if ($constructorArg->isDefaultValueAvailable()) {
+                        $return[] = $constructorArg->getDefaultValue();
+                    } else {
+                        throw $e;
+                    }
+                }
+            } else if ($constructorArg->isDefaultValueAvailable()) {
+                $return[] = $constructorArg->getDefaultValue();
+            } else {
+                $missing[] = $parameterName;
+            }
+        }
+
+        if (!empty($missing)) {
+            if (count($missing) === 1) {
+                throw new \InvalidArgumentException("Parameter {$missing[0]} is not set");
+            } else {
+                $missingParameters = join(', ', $missing);
+                throw new \InvalidArgumentException("Parameters {$missingParameters} are not set");
+            }
+        }
+
+        return $return;
+    }
+
+    public function instantiate(array $arguments)
+    {
+        if (!isset($this->constructorArgs)) {
+            //argument handling requires reflection in most cases
+            $this->reflector = new \ReflectionClass($this->className);
+
+            if (!$this->reflector->isInstantiable()) {
+                //interfaces does not need to be checked because class_exists doesn't allow them to get here
+                if ($this->reflector->isAbstract()) {
+                    throw new \InvalidArgumentException(
+                        "Cannot instantiate {$this->className} because the class is abstract"
+                    );
+                } else {
+                    throw new \InvalidArgumentException("Cannot instantiate {$this->className}");
+                }
+            }
+
+            $constructor = $this->reflector->getConstructor();
+
+            if ($constructor === null) {
+                $this->constructorArgs = [];
+            } else {
+                $this->constructorArgs = $constructor->getParameters();
+            }
+        }
+
+        if (empty($this->constructorArgs)) {
+            //Since the class has no constructor, it can not be instantiated with ReflectionClass
+
+            //Also, if the class has no constructor arguments, it is cheaper to instantiate it directly
+            //Although this has the downside of disallowing variable sized argument lists if
+            //no arguments are required by the constructor signature.
+            $object = new $this->className;
+        } else {
+            $arguments = $this->prepareParameters($this->constructorArgs, $arguments);
+
+            $object = $this->reflector->newInstanceArgs($arguments);
+        }
+
+        foreach ($this->callbacks as $callback) {
+            $callback($this->factory, $object);
+        }
+
+        return $object;
+    }
+}
--- src/Factory.php
+++ src/Factory.php
@@ -3,22 +3,25 @@
 namespace Factory;
 
 use Factory\Exceptions\InstantiationException;
-use ReflectionParameter;
 
-class Factory implements FactoryInterface
+class Factory
 {
-    private $constructorParameters = [];
+    /**
+     * @var Constructor[]
+     */
+    private $constructors    = [];
     private $storedInstances = [];
-    private $classNameMap = [];
-    private $callbacks = [];
+    private $classNameMap    = [];
 
     public function setAlias($className, $alias)
     {
         $this->stringTypeGuard($className);
         $this->stringTypeGuard($alias, 'alias');
 
-        if(isset($this->classNameMap[$className]) && $this->classNameMap[$className] === $alias) {
-            throw new \InvalidArgumentException("{$className} is already an alias of {$alias}, cannot set the reverse direction");
+        if (isset($this->classNameMap[ $className ]) && $this->classNameMap[ $className ] === $alias) {
+            throw new \InvalidArgumentException(
+                "{$className} is already an alias of {$alias}, cannot set the reverse direction"
+            );
         }
 
         $this->classNameMap[ $alias ] = $className;
@@ -42,16 +45,19 @@ class Factory implements FactoryInterface
     public function setParameters($className, array $parameters)
     {
         $this->stringTypeGuard($className);
-        $this->constructorParameters[ $className ] = $parameters;
+        if (!isset($this->constructors[ $className ])) {
+            $this->constructors[ $className ] = new Constructor($this, $className);
+        }
+        $this->constructors[ $className ]->setParameters($parameters);
     }
 
     public function addCallback($className, callable $callback)
     {
         $this->stringTypeGuard($className);
-        if (!isset($this->callbacks[ $className ])) {
-            $this->callbacks[ $className ] = [];
+        if (!isset($this->constructors[ $className ])) {
+            $this->constructors[ $className ] = new Constructor($this, $className);
         }
-        $this->callbacks[ $className ][] = $callback;
+        $this->constructors[ $className ]->addCallback($callback);
     }
 
     public function get($className, array $arguments = [], $forceNew = false)
@@ -67,119 +73,20 @@ class Factory implements FactoryInterface
             return $this->storedInstances[ $className ];
         }
 
+        if (!isset($this->constructors[ $className ])) {
+            $this->constructors[ $className ] = new Constructor($this, $className);
+        }
+
         try {
-            $object = $this->instantiate($className, $arguments);
+            $object = $this->constructors[ $className ]->instantiate($arguments);
         } catch (\Exception $e) {
             throw new InstantiationException("Could not instantiate {$className}", $e);
-        }
-        if (isset($this->callbacks[ $className ])) {
-            foreach ($this->callbacks[ $className ] as $callback) {
-                $callback($this, $object);
-            }
         }
         if (!$forceNew) {
             $this->storedInstances[ $className ] = $object;
         }
 
         return $object;
-    }
-
-    /**
-     * @param string $className
-     * @param \ReflectionParameter[] $constructorParameters
-     * @param array $parameters
-     * @return mixed
-     */
-    private function prepareParameters($className, $constructorParameters, $parameters)
-    {
-        $return  = [];
-        $missing = [];
-
-        if (isset($this->constructorParameters[ $className ])) {
-            $presetParameters = $this->constructorParameters[ $className ];
-        } else {
-            $presetParameters = [];
-        }
-
-        foreach ($constructorParameters as $constructorArg) {
-            $parameterName     = $constructorArg->getName();
-            $parameterPosition = $constructorArg->getPosition();
-
-            if (isset($parameters[ $parameterName ])) {
-                $return[] = $parameters[ $parameterName ];
-            } else if (isset($parameters[ $parameterPosition ])) {
-                $return[] = $parameters[ $parameterPosition ];
-            } else if (isset($presetParameters[ $parameterName ])) {
-                $return[] = $presetParameters[ $parameterName ];
-            } else if (isset($presetParameters[ $parameterPosition ])) {
-                $return[] = $presetParameters[ $parameterPosition ];
-            } else if ($constructorArg->getClass() !== null) {
-                try {
-                    $return[] = $this->get($constructorArg->getClass()->getName());
-                } catch (InstantiationException $e) {
-                    if ($constructorArg->isDefaultValueAvailable()) {
-                        $return[] = $constructorArg->getDefaultValue();
-                    } else {
-                        throw $e;
-                    }
-                }
-            } else if ($constructorArg->isDefaultValueAvailable()) {
-                $return[] = $constructorArg->getDefaultValue();
-            } else {
-                $missing[] = $parameterName;
-            }
-        }
-
-        if (!empty($missing)) {
-            if (count($missing) === 1) {
-                throw new \InvalidArgumentException("Parameter {$missing[0]} is not set");
-            } else {
-                $missingParameters = join(', ', $missing);
-                throw new \InvalidArgumentException("Parameters {$missingParameters} are not set");
-            }
-        }
-
-        return $return;
-    }
-
-    /**
-     * @param $className
-     * @param array $arguments
-     * @return mixed
-     */
-    private function instantiate($className, array $arguments)
-    {
-        if (!class_exists($className)) {
-            throw new \InvalidArgumentException("Class {$className} is not found");
-        }
-
-        //argument handling requires reflection in most cases
-        $reflector = new \ReflectionClass($className);
-
-        if (!$reflector->isInstantiable()) {
-            //interfaces does not need to be checked because class_exists doesn't allow them to get here
-            if ($reflector->isAbstract()) {
-                throw new \InvalidArgumentException("Cannot instantiate {$className} because the class is abstract");
-            } else {
-                throw new \InvalidArgumentException("Cannot instantiate {$className}");
-            }
-        }
-
-        $constructor = $reflector->getConstructor();
-
-        if ($constructor === null || $constructor->getNumberOfParameters() === 0) {
-            //Since the class has no constructor, it can not be instantiated with ReflectionClass
-
-            //Also, if the class has no constructor arguments, it is cheaper to instantiate it directly
-            //Although this has the downside of disallowing variable sized argument lists if
-            //no arguments are required by the constructor signature.
-            return new $className;
-        }
-
-        $constructorArgs = $constructor->getParameters();
-        $arguments       = $this->prepareParameters($className, $constructorArgs, $arguments);
-
-        return $reflector->newInstanceArgs($arguments);
     }
 
     /**
